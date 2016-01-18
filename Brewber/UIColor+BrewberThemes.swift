1095ca9efc7b2ea4d2d8e7d0bf9cc4ec2b862865
Alex Brashear <alexjbrashear@gmail.com>
2016-01-18 02:49:11 -0500

    Setup UIColor Extension

---
+++ Brewber/UIColor+BrewberThemes.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+BrewberThemes.swift
+//  Brewber
+//
+//  Created by Alex Brashear on 1/17/16.
+//  Copyright © 2016 Alex Brashear. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func brewberThemeColor() -> UIColor {
+        return UIColor(red: 1.0, green: 45.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+    }
+}
