b13296bee1585e07be7942ec67ae6bc2f4a6f021
Alex Brashear <alexjbrashear@gmail.com>
2016-01-20 00:07:42 -0500

    Tentative Model objects for user and authentication

---
+++ Brewber/BrewberAuth.swift
@@ -0,0 +1,37 @@
+//
+//  BrewberAuth.swift
+//  Brewber
+//
+//  Created by Alex Brashear on 1/19/16.
+//  Copyright © 2016 Alex Brashear. All rights reserved.
+//
+
+import UIKit
+
+class BrewberAuth: NSObject {
+    
+    // STUB: - Implement when token is available - fetches user from the keychain
+    class func currentUser() -> BrewberUser? {
+        return nil
+    }
+    
+    // STUB:
+    func isUserLoggedIn(user: BrewberUser) -> Bool {
+        return false
+    }
+    
+    // STUB:
+    func logourUser(user: BrewberUser) {
+        
+    }
+    
+    // STUB:
+    func loginUser(username: String, password: String) {
+        
+    }
+    
+    // STUB:
+    func signupUser(user: BrewberUser) {
+        
+    }
+}
---
+++ Brewber/BrewberUser.swift
@@ -0,0 +1,24 @@
+//
+//  BrewberUser.swift
+//  Brewber
+//
+//  Created by Alex Brashear on 1/19/16.
+//  Copyright © 2016 Alex Brashear. All rights reserved.
+//
+
+import UIKit
+
+class BrewberUser: NSObject {
+    
+    var phoneNumber: String!
+    var email: String!
+    var password: String!
+    var zipcode: String!
+    var birthdayMonth: Int!
+    var birthdayDay: Int!
+    
+    // STUB:
+    func isEqualToUser(user: BrewberUser) -> Bool {
+        return false
+    }
+}
