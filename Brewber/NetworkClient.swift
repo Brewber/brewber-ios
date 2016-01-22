9ef5c6e9f6878f0db08704d8b99ac8cab35a9d1e
Alex Brashear <alexjbrashear@gmail.com>
2016-01-22 00:41:34 -0500

    Stub out a potential network client, for future use, will probably end up using AlamoFire

---
+++ Brewber/NetworkClient.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkClient.swift
+//  Brewber
+//
+//  Created by Alex Brashear on 1/21/16.
+//  Copyright © 2016 Alex Brashear. All rights reserved.
+//
+
+import UIKit
+
+class NetworkClient: NSObject {
+    
+    let sharedClient = NetworkClient()
+
+    /// STUB
+    func enqueueRequest(requestItem: NetworkRequestItem, completion: ((NetworkResponse) -> ())) {
+        
+    }
+}
---
+++ Brewber/NetworkOperation.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkOperation.swift
+//  Brewber
+//
+//  Created by Alex Brashear on 1/22/16.
+//  Copyright © 2016 Alex Brashear. All rights reserved.
+//
+
+import UIKit
+
+class NetworkOperation: NSObject {
+
+}
