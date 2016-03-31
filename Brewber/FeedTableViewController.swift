bbc4575028b30f0e959566f83c7ee44a78bfef40
Alex Brashear <alexjbrashear@gmail.com>
2016-03-31 17:10:21 -0400

    Remove view logic moved to storyboard

--- Brewber/FeedTableViewController.swift
+++ Brewber/FeedTableViewController.swift
@@ -11,36 +11,9 @@ import UIKit
 class FeedTableViewController: UITableViewController {
     
     let FeedEventCellIdentifier = "FeedEventCellIdentifier"
-    let showSendBeerViewControllerSegue = "showSendBeerViewControllerSegue"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupNavigationBar()
-    }
-    
-    func setupNavigationBar() {
-        self.navigationController!.navigationBar.barTintColor = UIColor.vividRedColor()
-        self.title = "Feed"
-        self.navigationController?.navigationBar.translucent = false
-        
-        let titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        self.navigationController?.navigationBar.titleTextAttributes = titleTextAttributes
-        
-        let composeButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Compose, target: self, action: #selector(composeButtonPressed))
-        composeButton.tintColor = UIColor.whiteColor()
-        let settingsButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Organize, target: self, action: #selector(settingsButtonPressed))
-        settingsButton.tintColor = UIColor.whiteColor()
-        
-        self.navigationItem.rightBarButtonItem = composeButton
-        self.navigationItem.leftBarButtonItem = settingsButton
-    }
-    
-    func composeButtonPressed() {
-        self.performSegueWithIdentifier("showSendBeerViewControllerSegue", sender: self)
-    }
-    
-    func settingsButtonPressed() {
-        
     }
 
     // MARK: - Table view data source
@@ -61,42 +34,6 @@ class FeedTableViewController: UITableViewController {
 
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
