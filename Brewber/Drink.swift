65992d3f0aa743aa55fe7b1c701aa3ce7ba85820
Alex Brashear <alexjbrashear@gmail.com>
2016-04-05 12:33:19 -0400

    Add view controller and Drink and Bar model objects

---
+++ Brewber/Drink.swift
@@ -0,0 +1,13 @@
+//
+//  Drink.swift
+//  Brewber
+//
+//  Created by Alex Brashear on 4/5/16.
+//  Copyright © 2016 Alex Brashear. All rights reserved.
+//
+
+import Foundation
+
+struct Drink {
+    
+}
